2b030c19bd993c6917e5fee8b4edb1191ceebdab
Nampetch <nampetchbaby@gmail.com>
2021-12-08 13:36:23 +0700

    initial commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async ctx => {
-  ctx.body = 'Hello Petch';
+  ctx.body = 'Hello Petch 123';
 });
 
-app.listen(3000);
+app.listen(3000);S
